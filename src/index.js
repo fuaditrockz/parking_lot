17719a158387cc42aa9f4c752e84789189ec7641
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-11 22:23:50 +0700

    [src/index.js] Read text line by line on text file.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+const fs = require('fs')
+const readline = require('readline')
+const fileInput = 'file_input.txt'
+
+const readFileLineByLine = async () => {
+  const fileReadInterface = await readline.createInterface({
+    input: fs.createReadStream(fileInput)
+  })
+
+  return fileReadInterface.on('line', line => {
+    if (line === 'create_parking_lot 6') console.log('Created parking lot with 6 slots')
+    console.log(line)
+  })
+}
+
 const parkingLot = () => {
   console.log('ALL OUTPUT HERE!')
+  readFileLineByLine()
 }
 
 module.exports = parkingLot
