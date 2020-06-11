c2e47e4ba211d89f57169e568a7d767aa0380455
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-11 21:49:21 +0700

    [package.json & src/index.js] Remove prompt modules.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
-const prompt = require('prompt')
-
-prompt.start()
-
-let data = []
-
-const schema = {
-  properties: {
-    lot_available: {
-      required: true,
-      description: 'Lot Available'
-    }
-  }
-}
-
-const getParkingLotTotal = () => {
-  prompt.get(schema, (err, result) => {
-    if (err) return console.log(err)
-    console.log(`create_parking_lot: ${result.lot_available}`)
-  })
-}
-
 const parkingLot = () => {
-  getParkingLotTotal()
+  console.log('ALL OUTPUT HERE!')
 }
 
 module.exports = parkingLot
