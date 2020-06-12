88e7cfc59fabea91f622e0436939ec6f88f87522
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-13 02:45:51 +0700

    [src/index.js] Setup table cli.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const fs  = require('fs')
+const fs    = require('fs')
+const Table = require('cli-table3')
 const fileInput = 'file_input.txt'
 
 const TYPE_CONSTANTS = {
@@ -7,6 +8,11 @@ const TYPE_CONSTANTS = {
   LEAVE: 'leave',
   STATUS: 'status'
 }
+
+var table = new Table({
+  head: ['Slot No.', 'Registration No.']
+, colWidths: [10, 20]
+});
 
 const conditionType = (word, type) => {
   return word.includes(type)
@@ -33,7 +39,7 @@ const parkingLotProjectOutput = () => {
         totalParkingLots = totalParkingLots - 1
         const carIndexPosition = parkedCar.findIndex(value => value === carNumber)
         console.log(`Allocated slot number: ${carIndexPosition + 1}`)
-        console.log(totalParkingLots)
+        emptyLot > -1 ? table[emptyLot, emptyLot] = [carIndexPosition + 1, carNumber] : table.push([carIndexPosition + 1, carNumber])
       }
     }
     if (conditionType(data, TYPE_CONSTANTS.LEAVE)) {
@@ -51,8 +57,12 @@ const parkingLotProjectOutput = () => {
         const parkedHours = data.substr(data.length - 1)
         totalCharge = `is free with Charge $${(parkedHours - 2) * 10 + 10}`
         parkedCar[carIndexPosition] = null
+        table[carIndexPosition + 1, carNumber] = [null, null]
       }
       console.log(`Registration number ${carNumber()} with Slot Number ${carIndexPosition} ${totalCharge}`)
+    }
+    if (conditionType(data, TYPE_CONSTANTS.STATUS)) {
+      console.log(table.toString())
     }
   })
 }
