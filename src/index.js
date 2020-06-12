b0d4a1a365613f95a5378a67677b3fea51a43c9c
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-13 02:53:11 +0700

    [src/index.js] Separate each function inside condition.

--- src/index.js
+++ src/index.js
@@ -21,48 +21,73 @@ const conditionType = (word, type) => {
 let parkedCar = []
 let totalParkingLots
 
+// CREATE PARKING LOT
+const createLot = data => {
+  const total = parseInt(data.match(/\d+/))
+  totalParkingLots = total
+  console.log(`Created parking lot with ${total} slots`)
+}
+
+// PARKING CAR
+const parking = data => {
+  if (totalParkingLots < 1) {
+    console.log('Sorry, parking lot is full')
+  } else {
+    const carNumber = data.replace(TYPE_CONSTANTS.PARK + ' ', '')
+    const emptyLot = parkedCar.indexOf(null)
+    emptyLot > -1 ? parkedCar[emptyLot] = carNumber : parkedCar.push(carNumber)
+    totalParkingLots = totalParkingLots - 1
+    const carIndexPosition = parkedCar.findIndex(value => value === carNumber)
+    console.log(`Allocated slot number: ${carIndexPosition + 1}`)
+    emptyLot > -1 ? table[emptyLot, emptyLot] = [carIndexPosition + 1, carNumber] : table.push([carIndexPosition + 1, carNumber])
+  }
+}
+
+// LEAVING CAR
+const leaving = data => {
+  const carNumber = () => {
+    const removedTypeCommand = data.replace(TYPE_CONSTANTS.LEAVE + ' ', '')
+    return removedTypeCommand.slice(0, -2)
+  }
+
+  let carIndexPosition = parkedCar.findIndex(value => value === carNumber())
+  let totalCharge
+
+  if (carIndexPosition < 0) {
+    carIndexPosition = 'not found'
+    totalCharge = ''
+  } else {
+    totalParkingLots = totalParkingLots + 1
+    const parkedHours = data.substr(data.length - 1)
+    totalCharge = `is free with Charge $${(parkedHours - 2) * 10 + 10}`
+    parkedCar[carIndexPosition] = null
+    table[carIndexPosition + 1, carNumber] = [null, null]
+  }
+
+  console.log(`Registration number ${carNumber()} with Slot Number ${carIndexPosition} ${totalCharge}`)
+}
+
+// PRINT STATUS
+const status = () => {
+  console.log(table.toString())
+}
+
+// FINAL OUTPUT
 const parkingLotProjectOutput = () => {
-  const convertFileIntoArray = fs.readFileSync(fileInput).toString().split("\n");
+  const convertFileIntoArray = fs.readFileSync(fileInput).toString().split("\n")
+
   convertFileIntoArray.map(data => {
     if (conditionType(data, TYPE_CONSTANTS.CREATE_LOT)) {
-      const total = parseInt(data.match(/\d+/))
-      totalParkingLots = total
-      console.log(`Created parking lot with ${total} slots`)
+      createLot(data)
     }
     if (conditionType(data, TYPE_CONSTANTS.PARK) && !conditionType(data, TYPE_CONSTANTS.CREATE_LOT)) {
-      if (totalParkingLots < 1) {
-        console.log('Sorry, parking lot is full')
-      } else {
-        const carNumber = data.replace(TYPE_CONSTANTS.PARK + ' ', '')
-        const emptyLot = parkedCar.indexOf(null)
-        emptyLot > -1 ? parkedCar[emptyLot] = carNumber : parkedCar.push(carNumber)
-        totalParkingLots = totalParkingLots - 1
-        const carIndexPosition = parkedCar.findIndex(value => value === carNumber)
-        console.log(`Allocated slot number: ${carIndexPosition + 1}`)
-        emptyLot > -1 ? table[emptyLot, emptyLot] = [carIndexPosition + 1, carNumber] : table.push([carIndexPosition + 1, carNumber])
-      }
+      parking(data)
     }
     if (conditionType(data, TYPE_CONSTANTS.LEAVE)) {
-      const carNumber = () => {
-        const removedTypeCommand = data.replace(TYPE_CONSTANTS.LEAVE + ' ', '')
-        return removedTypeCommand.slice(0, -2)
-      }
-      let carIndexPosition = parkedCar.findIndex(value => value === carNumber())
-      let totalCharge
-      if (carIndexPosition < 0) {
-        carIndexPosition = 'not found'
-        totalCharge = ''
-      } else {
-        totalParkingLots = totalParkingLots + 1
-        const parkedHours = data.substr(data.length - 1)
-        totalCharge = `is free with Charge $${(parkedHours - 2) * 10 + 10}`
-        parkedCar[carIndexPosition] = null
-        table[carIndexPosition + 1, carNumber] = [null, null]
-      }
-      console.log(`Registration number ${carNumber()} with Slot Number ${carIndexPosition} ${totalCharge}`)
+      leaving(data)
     }
     if (conditionType(data, TYPE_CONSTANTS.STATUS)) {
-      console.log(table.toString())
+      status()
     }
   })
 }
